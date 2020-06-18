c6662925a5da6a7c77c36a817bb1d2b504e8ecd7
Harshad <harsh.haou@gmail.com>
2020-06-18 21:21:12 +0530

    third

--- src/App.js
+++ src/App.js
@@ -7,10 +7,19 @@ class App extends Component {
     <div className="app">
       <div class="container">
       <h2>Looking for cofounder </h2>
+      <p className='p'>Your life has been an inspiration to me. I tried to follow you. I tried to copy you. To summarize,
+        I was a fan of you. And I'm still. I've never seen someone as smart as you in my life. I think it
+        would be a mistake if I didn't ask you this in the first place before searching for anyone. <br/> <br/>
+        I know building a product is not an easy thing. Especially a product like this. I only have 1% of hope.
+         But let’s give a hard try.  If you think the same and you excited to be involved to solving this problem.
+        Please fill this form. 
+
+</p>
         <div class="row">
   
           <div className="formname">
           <form name="contact" method="POST" data-netlify="true">
+          <label for="male">Male</label>
             <p className="form-group">
               <input placeholder="Your name" className="form-control" type="text" name="name" />
             </p>
