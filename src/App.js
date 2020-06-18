4671b5c7c603eaa4aab9b183b1ea59e917eacc08
Harshad <harsh.haou@gmail.com>
2020-06-18 17:27:36 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+    <div className="app">
+      <div class="container">
+      <h2>Looking for cofounder </h2>
+        <div class="row">
+          
+        
+          <div className="formname">
+          <form name="contact" method="POST" data-netlify="true">
+            <p className="form-group">
+              <input placeholder="Your name" className="form-control" type="text" name="name" />
+            </p>
+            <p className="form-group">
+              <input placeholder="Your email" className="form-control" type="email" name="email" />
+            </p>
+
+            <p className="form-group">
+              <input placeholder="Equity shares" className="form-control" type="email" name="email" />
+            </p>
+
+            <p className="form-group">
+              <input placeholder="Subject" className="form-control" type="text" name="subject" />
+            </p>
+            
+            <p className="form-group">
+              <textarea placeholder="Message" cols="30" rows="7" className="form-control" name="message"></textarea>
+            </p>
+            <p className="form-group">
+              <button className="btn btn-primary py-3 px-5" type="submit">Send</button>
+            </p>
+          </form>
+          </div>
+          
+        
+       
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+      </div>
+    );
+  }
 }
 
 export default App;
