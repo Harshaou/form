fa2fb46e85d7ac7c158b73cec9d650812001ced7
Harshad <harsh.haou@gmail.com>
2020-06-26 08:13:32 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -27,22 +27,6 @@ handleIntrest = (val) => {
        <Route path='/form' component={Form} />
        <Route path='/thankyou' component={Thankyou} /> 
 
-       <form name="contact" method="post">
-          <p>
-            <label>Your Name: <input type="text" name="name"/></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email"/></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
-     
-
       </div>
     );
   }
